ffff22421ed93e9853de608a750ed3926b0f32f9
MahesaYogha <mahestar03@gmail.com>
2024-03-28 20:13:48 +0700

    PKD-29 sidebar

--- DEDIKASI_RPL_WebProject/resources/views/dashboard_peserta.blade.php
+++ DEDIKASI_RPL_WebProject/resources/views/dashboard_peserta.blade.php
@@ -1,3 +1,5 @@
+@include('sidebar/sidebar')
+
 <div class="page-wrapper">
         <div class="content container-fluid">
             <div class="page-header">
@@ -6,7 +8,7 @@
                         <div class="page-sub-header">
                             <h3 class="page-title">Welcome Bruklin!</h3>
                             <ul class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                                {{-- <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li> --}}
                                 <li class="breadcrumb-item active">Student</li>
                             </ul>
                         </div>
