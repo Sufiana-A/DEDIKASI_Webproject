a90dbab991e1517c554541ba560057844cedede6
arianaratnadewi <arinratnadewi@gmail.com>
2024-04-05 09:45:14 +0700

    PKD-10 Mengganti tipe data untuk view sign up peserta

--- DEDIKASI_RPL_WebProject/routes/web.php
+++ DEDIKASI_RPL_WebProject/routes/web.php
@@ -3,11 +3,25 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileAdminController;
+use App\Http\Controllers\ProfileMentorController;
 use App\Http\Controllers\ProfilePesertaController;
 
 Route::get('/', function () {
-    return view('register');
-})->name('viewRegister');
+    return view('dashboard/dashboard_peserta');
+});
+
+Route::controller(PelatihanController::class)->group(function () {
+    Route::get('pelatihan/list/page', 'pelatihanList')->middleware('auth')->name('pelatihan/list/page'); // pelatihan/list/page
+    Route::get('pelatihan/add/page', 'pelatihanAdd')->middleware('auth')->name('pelatihan/add/page'); // pelatihan/add/page
+    Route::post('pelatihan/save', 'savePelatihan')->name('pelatihan/save'); // pelatihan/save
+    Route::post('pelatihan/update', 'updatePelatihan')->name('pelatihan/update'); // pelatihan/update
+    Route::post('pelatihan/delete', 'deletePelatihan')->name('pelatihan/delete'); // pelatihan/delete
+    Route::get('pelatihan/edit/{id_pelatihan}', 'pelatihanEdit'); // pelatihan/edit/page
+});
+
+    // return view('register');
+    // ->name('viewRegister');
 
 //Route::get('/register', [RegisterController::class, 'register']);
 Route::post('/register', [RegisterController::class, 'register'])->name('register');
@@ -15,9 +29,22 @@ Route::post('/register', [RegisterController::class, 'register'])->name('registe
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/loginsubmit', [LoginController::class, 'loginvalid'])->name('loginsubmit');
 
-Route::get('/profil-peserta', [ProfilePesertaController::class, 'show_profile'])->name('profil-peserta');
-
-
 
 Route::post('/login', [LoginController::class, 'index'])->name('login');
+
+//peserta
+Route::get('/profile-peserta', [ProfilePesertaController::class, 'show_profile'])->name('profile_peserta');
+Route::get('/profile-peserta-edit', [ProfilePesertaController::class, 'edit_profile'])->name('profile_edit_peserta');
+Route::post('/profile-peserta-submit', [ProfilePesertaController::class, 'submit_profile'])->name('profile_submit');
+Route::post('/profile-photo-peserta-submit', [ProfilePesertaController::class, 'submit_photo'])->name('profil_photo_submit');
+
+//mentor
+Route::get('/profile-mentor', [ProfileMentorController::class, 'show_profile'])->name('profile_mentor');
+Route::get('/profile-mentor-edit', [ProfileMentorController::class, 'edit_profile'])->name('profile_edit_mentor');
+
+
+//admin
+Route::get('/profile-admin', [ProfileAdminController::class, 'show_profile'])->name('profile_admin');
+Route::get('/profile-admin-edit', [ProfileAdminController::class, 'edit_profile'])->name('profile_edit_admin');
+
 
