b2a1e5f1c65b9c08a99114c564b0a5968e4f1c21
dpmarthin <dpmarthin@student.telkomuniversity.ac.id>
2024-04-29 22:39:48 +0700

    PKD-22 Update Blade Peserta Pelatihan

--- DEDIKASI_RPL_WebProject/resources/views/peserta_pelatihan/listPesertaPelatihan.blade.php
+++ DEDIKASI_RPL_WebProject/resources/views/peserta_pelatihan/listPesertaPelatihan.blade.php
@@ -14,13 +14,15 @@
                 @foreach($pelatihanAcc as $enroll)
                 <div class="row row-cols-md-2">
                     <div class="col mb-4">
-                        <div class="card bg-primary text-white shadow mb-4">
+                        <div class="card bg-primary text-black shadow mb-4">
                             <div class="card-body">
-                                <h5 class="card-title">{{ $enroll->courses->title }}</h5>
-                                <p class="card-text">{{ $enroll->courses->description }}</p>
-                                <a href="#" class="btn btn-outline-dark"><i class="fas fa-video"></i> Video </a>
-                                <a href="#" class="btn btn-outline-dark"><i class="fas fa-file"></i> Material </a>
-                                <a href="#" class="btn btn-outline-dark"><i class="fas fa-file-alt"></i> Assignment </a>
+                                <h5 class="card-title">{{ $enroll->course->title }}</h5>
+                                <p class="card-text">{{ $enroll->course->class }}</p>
+                                <img src="{{ asset('assets/img/peserta_pelatihan/'.Auth::guard('course')->user()->image) }}" class="card-img-top" style="width:400px" alt="figma">
+                                <p class="card-text">{{ $enroll->course->description }}</p>
+                                <a href="{{ $enroll->video->id_video }}" class="btn btn-primary"><i class="fas fa-video"></i> Video </a>
+                                <a href="{{ $enroll->materi->id_materi }}" class="btn btn-primary"><i class="fas fa-file"></i> Material </a>
+                                <a href="{{ $enroll->assignment->id_tugas }}" class="btn btn-primary"><i class="fas fa-file-alt"></i> Assignment </a>
                             </div>
                         </div>
                     </div>
