d22afabd160183329d3043598a11af334730dbff
MahesaYogha <mahestar03@gmail.com>
2024-05-07 18:41:26 +0700

    PKD-43 Controller, Migration, Model fitur nilai

---
+++ DEDIKASI_RPL_WebProject/database/migrations/2024_05_07_113313_create_assignment_peserta.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->nullable();
+            $table->string('title')->nullable();
+            $table->string('class')->nullable();
+            $table->longText('description')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('grades');
+    }
+};
