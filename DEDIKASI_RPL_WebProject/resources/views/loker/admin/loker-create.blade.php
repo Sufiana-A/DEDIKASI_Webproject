d4f3858a3ce690b6677d4818984142372bfa8cb8
arianaratnadewi <arinratnadewi@gmail.com>
2024-05-21 09:47:25 +0700

    PKD-53 menambah migration, controller, dan view untuk tambah lowongan kerja

---
+++ DEDIKASI_RPL_WebProject/resources/views/loker/admin/loker-create.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.adminmaster')
+
+@section('content')
+<div class="page-wrapper">
+    <div class="content container-fluid">
+        <div class="row">
+        <div class="col-md-12">
+         <div class="card">
+    <div class="container">
+        <h2 style="margin-top: 20px;">Tambah Lowongan Baru</h2>
