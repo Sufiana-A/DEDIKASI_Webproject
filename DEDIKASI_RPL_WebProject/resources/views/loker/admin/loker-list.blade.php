68ccbfd57114d3bc8defebac75af7a802b411455
arianaratnadewi <arinratnadewi@gmail.com>
2024-05-21 09:58:43 +0700

    PKD-56 menambah view untuk list pelatihan yang dapat menghapus

---
+++ DEDIKASI_RPL_WebProject/resources/views/loker/admin/loker-list.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.adminmaster')
+@section('content')
+    <div class="page-wrapper">
+        <div class="content container-fluid">
+
+            <div class="page-header">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h3 class="page-title">LIST LOWONGAN KERJA</h3>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Lowongan Kerja</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
