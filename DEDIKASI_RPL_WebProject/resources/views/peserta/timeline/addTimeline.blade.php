b92abe8647d136bfa719ff6825dfecefca3d451a
dpmarthin <dpmarthin@student.telkomuniversity.ac.id>
2024-05-31 16:31:49 +0700

    PKD-31 Update Blade

--- DEDIKASI_RPL_WebProject/resources/views/peserta/timeline/addTimeline.blade.php
+++ DEDIKASI_RPL_WebProject/resources/views/peserta/timeline/addTimeline.blade.php
@@ -1,4 +1,5 @@
 @extends('layout.master')
+@section('title', 'Add Timeline')
 @section('content')
 {{-- message --}}
 
--- DEDIKASI_RPL_WebProject/resources/views/peserta/timeline/listTimeline.blade.php
+++ DEDIKASI_RPL_WebProject/resources/views/peserta/timeline/listTimeline.blade.php
@@ -1,4 +1,5 @@
 @extends('layout.master')
+@section('title', 'List Timeline')
 @section('content')
 {{-- message --}}
 
