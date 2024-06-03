86c4e5999cdce6135be82d7e1a0899619b15d77e
dpmarthin <dpmarthin@student.telkomuniversity.ac.id>
2024-06-03 15:32:52 +0700

    PKD-22 Update Testing

---
+++ DEDIKASI_RPL_WebProject/tests/Browser/ViewListPelatihan1Test.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ViewListPelatihan1Test extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     * @group viewListPelatihan1
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                    ->assertSee('Welcome to Dashboard')
+                    ->type('email', 'dyom@gmail.com')
+                    ->type('password', 'dyomentor123')
+                    ->press('Login')
+                    ->assertPathIs('/dashboard-peserta')
+                    ->assertSee('Management')
+                    ->clickLink('List Pelatihan')
+                    ->assertPathIs('/peserta-pelatihan');
+        });
+    }
+}
--- DEDIKASI_RPL_WebProject/tests/Browser/ViewListPelatihan2Test.php
+++ DEDIKASI_RPL_WebProject/tests/Browser/ViewListPelatihan2Test.php
@@ -6,19 +6,17 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
-class ViewListPelatihanTest extends DuskTestCase
+class ViewListPelatihan2Test extends DuskTestCase
 {
     /**
      * A Dusk test example.
-     * @group viewListPelatihan
+     * @group viewListPelatihan2
      */
     public function testExample(): void
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/dashboard-peserta')
-                    ->assertSee('Management')
-                    ->clickLink('List Pelatihan')
-                    ->assertPathIs('/peserta-pelatihan');
+            $browser->visit('/peserta-pelatihan')
+                    ->assertSee('Pelatihan Saya');
         });
     }
 }
