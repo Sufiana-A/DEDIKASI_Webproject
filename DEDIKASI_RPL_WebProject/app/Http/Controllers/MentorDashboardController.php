c6970c390ecad0be512338eed8f817343f0afa69
dpmarthin <dpmarthin@student.telkomuniversity.ac.id>
2024-06-04 21:12:30 +0700

    PKD-52 Update Controller, Models, Blade

--- DEDIKASI_RPL_WebProject/app/Http/Controllers/MentorDashboardController.php
+++ DEDIKASI_RPL_WebProject/app/Http/Controllers/MentorDashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use App\Models\Mentor;
 use App\Models\Course;
 use App\Models\Peserta;
@@ -13,15 +14,27 @@ class MentorDashboardController extends Controller
     public function index()
     {
         $mentorId = Auth::guard('mentor')->user()->id;
-        $courses = Course::where('mentor_id', $mentorId)->get();
-
-        $pesertaId = [];
-        foreach ($courses as $course) {
-            $pesertaId = array_merge($pesertaId, $course->Peserta()->wherePivot('status', 'Diterima')->pluck('peserta_id')->toArray());
-        }
-
-        $jumlah_peserta = count(array_unique($pesertaId));
-
-        return view('dashboard.mentor_dashboard', compact('courses', 'jumlah_peserta'));
+        
+        // PKD-51
+        $jumlah_peserta = DB::table('courses')
+                        ->join('peserta_course', 'courses.id', '=', 'peserta_course.course_id')
+                        ->join('peserta', 'peserta_course.peserta_id', '=', 'peserta.id')
+                        ->select('courses.mentor_id', DB::raw('count(distinct peserta.id) as jumlah_diterima'))
+                        ->where('peserta_course.status', 'Diterima')
+                        ->where('courses.mentor_id', $mentorId)
+                        ->groupBy('courses.mentor_id')
+                        ->get();
+    
+        // PKD-52
+        $courses = DB::table('courses')
+                    ->join('peserta_course', 'courses.id', '=', 'peserta_course.course_id')
+                    ->join('peserta', 'peserta_course.peserta_id', '=', 'peserta.id')
+                    ->select('courses.id', 'courses.title', 'courses.class', 'courses.description', 'courses.mentor_id', DB::raw('count(distinct peserta.id) as jumlah_diterima'))
+                    ->where('peserta_course.status', 'Diterima')
+                    ->where('courses.mentor_id', $mentorId)
+                    ->groupBy('courses.id', 'courses.title', 'courses.class', 'courses.description', 'courses.mentor_id')
+                    ->get();
+    
+        return view('dashboard.mentor_dashboard', compact('jumlah_peserta', 'courses'));
     }    
 }
